0cbb58d711a8c0fb845b89fd33d6082a47fdf80a
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-15 14:15:36 -0400

    Added boiler plate to files

--- NBAStats/models.py
+++ NBAStats/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    
+    def __str__(self):
+        return self.username
---
+++ NBAStats/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'', )
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
